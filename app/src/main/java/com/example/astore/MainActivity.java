a60689ba4451a42c405f38b90d46ce149e56366c
jawad mughal <mughaljawad12@gmail.com>
2023-12-06 10:43:06 +0500

    tool bar

--- app/src/main/java/com/example/astore/MainActivity.java
+++ app/src/main/java/com/example/astore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.astore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,28 +11,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     ArrayList<productModel> product = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // create a tool bar
+        toolbar = findViewById(R.id.toolBar);
+
+        //setup 1
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         RecyclerView recyclerView = findViewById(R.id.rvProduct);
 
         recyclerView.setLayoutManager(new GridLayoutManager( this , 2));
 
         // Create productModel instances with random data and different drawables
-        product.add(new productModel(R.drawable.e , "product 1" , "800", "300", 4.4F));
-        product.add(new productModel(R.drawable.e , "product 2" , "600", "250", 4.0F));
-        product.add(new productModel(R.drawable.e , "product 3" , "900", "400", 4.8F));
-        product.add(new productModel(R.drawable.e , "product 4" , "700", "350", 4.2F));
-        product.add(new productModel(R.drawable.e , "product 5" , "1000", "500", 4.5F));
-        product.add(new productModel(R.drawable.e , "product 6" , "1200", "600", 4.6F));
-        product.add(new productModel(R.drawable.e , "product 7" , "500", "200", 3.9F));
-        product.add(new productModel(R.drawable.e , "product 8" , "1100", "450", 4.7F));
-        product.add(new productModel(R.drawable.e , "product 9" , "850", "320", 4.3F));
-        product.add(new productModel(R.drawable.e , "product 10", "950", "380", 4.1F));
+        product.add(new productModel(R.drawable.e , "Product 1" , "Rs 800", "Rs 300", 4.4F));
+        product.add(new productModel(R.drawable.e , "Product 2" , "Rs 600", "Rs 250", 4.0F));
+        product.add(new productModel(R.drawable.e , "Product 3" , "Rs 900", "Rs 400", 4.8F));
+        product.add(new productModel(R.drawable.e , "Product 4" , "Rs 700", "Rs 350", 4.2F));
+        product.add(new productModel(R.drawable.e , "Product 5" , "Rs 1000", "Rs 500", 4.5F));
+        product.add(new productModel(R.drawable.e , "Product 6" , "Rs 1200", "Rs 600", 4.6F));
+        product.add(new productModel(R.drawable.e , "Product 7" , "Rs 500", "Rs 200", 3.9F));
+        product.add(new productModel(R.drawable.e , "Product 8" , "Rs 1100", "Rs 450", 4.7F));
+        product.add(new productModel(R.drawable.e , "Product 9" , "Rs 850", "Rs 320", 4.3F));
+        product.add(new productModel(R.drawable.e , "Product 10", "Rs 950", "Rs 380", 4.1F));
+
 
 
         RecyclerProductAdaptor adaptor = new RecyclerProductAdaptor(this, product);
