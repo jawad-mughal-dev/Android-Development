078269a6c5d14ffded78787e155229ff69cf2e06
jawad mughal <mughaljawad12@gmail.com>
2023-12-06 09:08:50 +0500

    dialog layout

--- app/src/main/java/com/example/astore/MainActivity.java
+++ app/src/main/java/com/example/astore/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<productModel> product = new ArrayList<>();
+
+    RecyclerProductAdaptor adaptor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,82 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.rvProduct);
 
         recyclerView.setLayoutManager(new GridLayoutManager( this , 2));
+
+        // create a floating product for attached different actions
+        FloatingActionButton fbForProdudct = findViewById(R.id.fbForProdudct);
+
+
+        fbForProdudct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialoglayoutforupdateproduct);
+
+                //Find ids becuase we want to get data from
+                TextView tvProductFormName = dialog.findViewById(R.id.tvProductFormName);
+                EditText etProductName = dialog.findViewById(R.id.etProductName);
+                EditText etProductPrice = dialog.findViewById(R.id.etProductPrice);
+                Button btnSubmitProduct = dialog.findViewById(R.id.btnSubmitProduct);
+
+
+
+
+
+                // we need to get data on the form click
+                btnSubmitProduct.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        // very import give the intial value
+                        String name = "" , price = "";
+                        if (!etProductName.getText().toString().equals("")) {
+                            name = etProductName.getText().toString();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please Enter Product Name", Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!etProductPrice.getText().toString().equals("")) {
+                            price = etProductPrice.getText().toString();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please Enter Product Price", Toast.LENGTH_SHORT).show();
+                        }
+                        product.add(new productModel( name , price , R.drawable.e ));
+                        adaptor.notifyItemInserted(product.size()-1);
+                        recyclerView.scrollToPosition(product.size()-1);
+
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // Create productModel instances with random data and different drawables
         product.add(new productModel(R.drawable.e , "product 1" , "800", "300", 4.4F));
@@ -34,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         product.add(new productModel(R.drawable.e , "product 10", "950", "380", 4.1F));
 
 
-        RecyclerProductAdaptor adaptor = new RecyclerProductAdaptor(this, product);
+         adaptor = new RecyclerProductAdaptor(this, product);
         recyclerView.setAdapter(adaptor);
     }
 }
